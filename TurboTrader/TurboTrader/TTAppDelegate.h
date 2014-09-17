240f06bb95290c101f39756d101c1131bc298fc2
Joe Nash <joe.nash@gamesys.co.uk>
2014-09-17 12:50:01 +0100

    Added initial Xcode project

---
+++ TurboTrader/TurboTrader/TTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TTAppDelegate.h
+//  TurboTrader
+//
+//  Created by Joe Nash on 17/09/2014.
+//  Copyright (c) 2014 Joe Nash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TurboTrader/TurboTrader/TTViewController.m
@@ -0,0 +1,29 @@
+//
+//  TTViewController.m
+//  TurboTrader
+//
+//  Created by Joe Nash on 17/09/2014.
+//  Copyright (c) 2014 Joe Nash. All rights reserved.
+//
+
+#import "TTViewController.h"
+
+@interface TTViewController ()
+
+@end
+
+@implementation TTViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
